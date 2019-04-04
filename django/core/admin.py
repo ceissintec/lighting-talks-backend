e05e522908ab8a5e4d22fb322d8b3bcd5cf91ba2
Roniel Valdez <roniel_valdez@outlook.com>
2019-04-03 22:50:27 -0400

    Added Submission model

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from lighting_talks.models import Submission
 
 # Register your models here.
+
+admin.site.register(Submission)
--- django/lighting_talks/models.py
+++ django/lighting_talks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Submission(models.Model):
+    first_name = models.CharField(max_length=125)
+    last_name = models.CharField(max_length=125)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_accepted = models.BooleanField(default=False)
+    scheduled_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
